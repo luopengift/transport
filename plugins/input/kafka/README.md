f67b1aeabf38badb3c2ac857c33f2f9823a883ba
root <root@ip-10-10-20-20.ec2.internal>
2017-08-25 19:50:24 +0800

    Update input kafka plugin: Add group option

--- plugins/input/kafka/README.md
+++ plugins/input/kafka/README.md
@@ -7,8 +7,17 @@
             "192.168.0.2:9092",
             "192.168.0.3:9092"
         ],
-        "topic": "test",
-        "offset": -1
+        "topics": [
+            "test"
+        ],
+        "group":"transport",
+        "offset": -1    # -1:newest,-2:oldest
     }
 }
-```
+
+| 参数 | 类型 | 要求 | 说明 |
+| -----| ---- | ---- |------|
+|addrs |list  | Y    |若group为空，addrs为kafka brokers的地址；否则为zookeeper的地址 |
+|topics|list  | Y    |kafka topic list| 
+|group |string| N    |kafka group|
+|offset|int   | Y    |-1: newest, -2: oldest|
--- plugins/input/kafka/kafka.go
+++ plugins/input/kafka/kafka.go
@@ -4,7 +4,10 @@ import (
 	"github.com/Shopify/sarama"
 	"github.com/luopengift/golibs/logger"
 	"github.com/luopengift/transport"
+    "github.com/wvanbergen/kafka/consumergroup"
+    "github.com/wvanbergen/kazoo-go"
 	"sync"
+    "time"
 )
 
 /*
@@ -13,8 +16,9 @@ sarame.OffsetNewest int64 = -1
 sarame.OffsetOldest int64 = -2
 */
 type KafkaInput struct {
-	Addrs  []string `json:"addrs"`
-	Topic  string   `json:"topic"`
+	Addrs  []string `json:"addrs"` //如果定义了group,则addrs是zookeeper的地址(2181)，否则的话是kafka的地址(9092)
+	Topics []string `json:"topics"`
+	Group  string   `json:"group"`
 	Offset int64    `json:"offset"`
 
 	Message chan []byte //从这个管道中读取数据
@@ -31,7 +35,7 @@ func (in *KafkaInput) Init(config transport.Configer) error {
 		logger.Error("parse error:%v", err)
 		return err
 	}
-	in.Message = make(chan []byte)
+	in.Message = make(chan []byte, 1000)
 	return nil
 }
 
@@ -42,22 +46,29 @@ func (in *KafkaInput) Read(p []byte) (cnt int, err error) {
 }
 
 func (in *KafkaInput) Start() error {
-	in.ReadFromTopic()
+	if in.Group == "" {
+		for _, topic := range in.Topics {
+			go in.ReadFromTopic(topic)
+		}
+	} else {
+		in.ReadWithGroup()
+	}
 	return nil
 }
 
-func (in *KafkaInput) ReadFromTopic() {
+// 简单kafka消费者
+func (in *KafkaInput) ReadFromTopic(topic string) {
 	var wg sync.WaitGroup
 	consumer, err := sarama.NewConsumer(in.Addrs, sarama.NewConfig())
 	if err != nil {
 		logger.Error("<new consumer error> %v", err)
 	}
-	partitionList, err := consumer.Partitions(in.Topic)
+	partitionList, err := consumer.Partitions(topic)
 	if err != nil {
 		logger.Error("<consumer partitions> %v", err)
 	}
 	for partition := range partitionList {
-		pc, err := consumer.ConsumePartition(in.Topic, int32(partition), in.Offset)
+		pc, err := consumer.ConsumePartition(topic, int32(partition), in.Offset)
 		if err != nil {
 			logger.Error("<consume error> %v", err)
 		}
@@ -74,6 +85,51 @@ func (in *KafkaInput) ReadFromTopic() {
 	}
 	wg.Wait()
 	consumer.Close()
+}
+
+// 多个consumer group
+func (in *KafkaInput) ReadWithGroup() error {
+	config := consumergroup.NewConfig()
+	config.Offsets.Initial = in.Offset
+	config.Offsets.ProcessingTimeout = 10 * time.Second
+
+	zookeeperNodes, chroot := kazoo.ParseConnectionString(in.Addrs[0])
+	config.Zookeeper.Chroot = chroot
+	consumer, err := consumergroup.JoinConsumerGroup(in.Group, in.Topics, zookeeperNodes, config)
+	if err != nil {
+		logger.Error("parse error:%v", err)
+		return err
+	}
+
+	go func() {
+		for err := range consumer.Errors() {
+			logger.Error("consumer error:%v", err)
+		}
+	}()
+
+	eventCount := 0
+	offsets := make(map[string]map[int32]int64)
+
+	for message := range consumer.Messages() {
+		if offsets[message.Topic] == nil {
+			offsets[message.Topic] = make(map[int32]int64)
+		}
+
+		eventCount += 1
+		if offsets[message.Topic][message.Partition] != 0 && offsets[message.Topic][message.Partition] != message.Offset-1 {
+			logger.Error("Unexpected offset on %s:%d. Expected %d, found %d, diff %d.", message.Topic, message.Partition, offsets[message.Topic][message.Partition]+1, message.Offset, message.Offset-offsets[message.Topic][message.Partition]+1)
+		}
+
+		offsets[message.Topic][message.Partition] = message.Offset
+		consumer.CommitUpto(message)
+		in.Message <- message.Value
+	}
+
+	logger.Info("Processed %d events.", eventCount)
+	logger.Info("%+v", offsets)
+	consumer.Close()
+	return nil
+
 }
 
 func (in *KafkaInput) Close() error {
