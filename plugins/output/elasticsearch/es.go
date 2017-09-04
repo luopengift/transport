b5a781d5e5a832fa1dd37988733e630b5448cd85
root <root@iZm5egf7xb48axmu4z1t3fZ.(none)>
2017-09-04 15:07:32 +0800

    Update output es plugin

--- plugins/output/elasticsearch/es.go
+++ plugins/output/elasticsearch/es.go
@@ -1,11 +1,14 @@
 package elasticsearch
 
 import (
-	"bytes"
-	"github.com/luopengift/gohttp"
 	"github.com/luopengift/golibs/file"
 	"github.com/luopengift/golibs/logger"
 	"github.com/luopengift/transport"
+
+    "context"
+    es "gopkg.in/olivere/elastic.v5"
+
+
 )
 
 type EsOutput struct {
@@ -15,8 +18,9 @@ type EsOutput struct {
 	Timeout int      `json:"time"`  //Pool timeout
 	Batch   int      `json:"batch"` //多少条数据提交一次
 
-	Pool   *gohttp.ClientPool
-	Buffer chan []byte
+    buffer chan []byte
+    ctx context.Context
+    client  *es.Client
 }
 
 func NewEsOutput() *EsOutput {
@@ -30,38 +34,51 @@ func (out *EsOutput) Init(config transport.Configer) error {
 	if err != nil {
 		return err
 	}
-	out.Pool = gohttp.NewClientPool(5, 50, out.Timeout)
-	out.Buffer = make(chan []byte, out.Batch*2)
-	return err
+
+    out.buffer = make(chan []byte, out.Batch * 2)
+
+    // 连接es 
+    out.ctx = context.Background()
+    out.client, err = es.NewClient(es.SetURL("http://"+out.Addrs[0]), es.SetSniff(false))
+    if err != nil {
+        return err
+    }
+
+    // 检查index是否存在，如果不存在则创建index 
+    exists, err := out.client.IndexExists(file.TimeRule.Handle(out.Index)).Do(out.ctx)
+    if err != nil {
+        return err
+    }
+    if !exists {
+        _, err := out.client.CreateIndex(file.TimeRule.Handle(out.Index)).Do(out.ctx)
+        if err != nil {
+            return err
+        }
+    }
+    return nil
+
 }
 
 func (out *EsOutput) Write(p []byte) (int, error) {
-	bulk := NewBulkIndex(file.TimeRule.Handle(out.Index), out.Type, "", p)
-	b, err := bulk.Bytes()
-	if err != nil {
-		return 0, err
-	}
-	out.Buffer <- b
-	return len(b), nil
+	out.buffer <- p
+	return len(p), nil
 }
 
 func (out *EsOutput) Start() error {
-	cnt := 0
-	var buf bytes.Buffer
-	for b := range out.Buffer {
-		if cnt == out.Batch {
-			logger.Info("send:%v", string(buf.Bytes()))
-			err := Send(out.Addrs[0], buf.Bytes())
-			if err != nil {
-				logger.Error("send bulk error,%v", err)
-			}
-			buf.Reset()
-			cnt = 0
-		}
-		buf.Write(b)
-		cnt = cnt + 1
-	}
-	return nil
+    for {
+        bulkRequest := out.client.Bulk()
+        for tmp := out.Batch; tmp>0; tmp-- {
+            req := es.NewBulkIndexRequest().Index(out.Index).Type(out.Type).Doc(string(<-out.buffer))
+            bulkRequest.Add(req)
+        }
+        bulkResponse, err := bulkRequest.Do(out.ctx)
+        if err != nil {
+            logger.Error("bulkResponse error %v",err)
+        }
+        indexed := bulkResponse.Indexed() 
+        logger.Info("导入了",len(indexed),"条数据") 
+    }
+    return nil
 }
 
 func (out *EsOutput) Close() error {
