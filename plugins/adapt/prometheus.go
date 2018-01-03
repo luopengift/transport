cc73789943d1550888ccc434a231da8ad6a0f55f
root <root@ip-10-100-50-218.cn-north-1.compute.internal>
2018-01-03 13:56:54 +0800

    update

--- plugins/adapt/prometheus.go
+++ plugins/adapt/prometheus.go
@@ -1,18 +1,20 @@
 package codec
 
 import (
+	"time"
+	"fmt"
 	"github.com/luopengift/transport"
 	"github.com/luopengift/types"
+	"strings"
 )
 
 type Alert struct {
-	Labels	map[string]string		`json:"labels"`
-	Annotations	map[string]string	`json:"annotations"`
-	StartsAt	string			`json:"startsAt,omitempty"`
-	EndsAt		string			`json:"endsAt,omitempty"`
-	GeneratorURL	string			`json:"generatorURL,omitempty"`
+	Labels       map[string]string `json:"labels"`
+	Annotations  map[string]string `json:"annotations"`
+	StartsAt     time.Time         `json:"startsAt,omitempty"`
+	EndsAt       time.Time         `json:"endsAt,omitempty"`
+	GeneratorURL string            `json:"generatorURL"`
 }
-
 
 // add a enter symbol at end of line, classic written into file
 type PrometheusAlertHandler struct{}
@@ -21,34 +23,64 @@ func (h *PrometheusAlertHandler) Init(config transport.Configer) error {
 	return nil
 }
 
+func timeformat(t string) string {
+	zone, _ := time.LoadLocation("Asia/Chongqing")
+	loctime, _ := time.ParseInLocation("2006-01-02T15:04:05.000Z", t, zone)
+	return loctime.Add(8 * time.Hour).Format(time.RFC822)
+}
+
+func timeformat2(t string) time.Time {
+	zone, _ := time.LoadLocation("Asia/Chongqing")
+	loctime, _ := time.ParseInLocation("2006-01-02T15:04:05.000Z", t, zone)
+	return loctime.Add(8 * time.Hour)
+}
+
 func (h *PrometheusAlertHandler) Handle(in, out []byte) (int, error) {
 	src, err := types.ToMap(in)
 	if err != nil {
-		return 0, err
+		return 0, fmt.Errorf("%v => %v", err, string(in))
 	}
+	file := ""
+	value, ok := src["source"]
+	if ok {
+		file = value.(string)
+		//return 0, fmt.Errorf("missing source field!")
+	}
+	host := src["beat"].(map[string]interface{})["hostname"].(string)
+	service := ""
+	services := strings.Split(file, "/")
+	if len(services) < 7 {
+		service = file
+	} else {
+		service = services[7]
+	}
+	url := "http://10.28.13.66:5601/app/kibana#/discover?_g=(refreshInterval:(display:Off,pause:!f,value:0),time:(from:now-24h,mode:quick,to:now))&_a=(columns:!(_source),index:'logstash-*',interval:auto,query:(query_string:(analyze_wildcard:!t,query:'serviceName:%22"+service+"%22%20AND%20host:%22"+host+"%22%20AND%20level:%22ERROR%22')),sort:!('@timestamp',desc))"
 	labels := map[string]string{
 		"alertname": "ERROR_LOG",
-		"service": src["serviceName"].(string),//"error_log"
-		"file":src["source"].(string),
-		"host": src["beat"].(map[string]interface{})["hostname"].(string),
+		"service":   service, //"error_log"
+		"file":      src["source"].(string),
+		"host":      host,
+	//	"starttime": timeformat(src["@timestamp"].(string)),
 	}
 	annotations := map[string]string{
 		"summary": src["message"].(string),
 	}
-	for k, v := range src["error_stack"].(map[string]interface{}) {
-		annotations[k] = v.(string)
+	if err_stack, ok := src["error_stack"]; ok {
+		for k, v := range err_stack.(map[string]interface{}) {
+			annotations[k] = v.(string)
+		}
 	}
 	dest := Alert{
-		Labels: labels,
+		Labels:      labels,
 		Annotations: annotations,
-		StartsAt: src["@timestamp"].(string),
+		StartsAt:    timeformat2(src["@timestamp"].(string)),
 		//EndsAt: "0001-01-01T00:00:00Z",
-		GeneratorURL: "",
+		GeneratorURL: url,
 	}
 	alerts := []*Alert{&dest}
 	b, err := types.ToBytes(alerts)
 	if err != nil {
-		return 0, nil
+		return 0, fmt.Errorf("%v => %v", err, string(in))
 	}
 	n := copy(out, b)
 	return n, nil
--- transport.go
+++ transport.go
@@ -3,9 +3,9 @@ package transport
 import (
 	"fmt"
 	"github.com/luopengift/golibs/logger"
+	"os"
 	"strings"
 	"time"
-	"os"
 )
 
 type Transport struct {
@@ -41,7 +41,7 @@ func NewTransport(cfg *Config) (*Transport, error) {
 	transport.recvChan = make(chan []byte, transport.chanSize)
 	transport.sendChan = make(chan []byte, transport.chanSize)
 	transport.isEnd = make(chan bool)
-	f := os.Stdout//logger.NewFileWriter("logs/transport_%Y%M%D.log", 1024 * 1024 * 1024)
+	f := os.Stdout //logger.NewFileWriter("logs/transport_%Y%M%D.log", 1024 * 1024 * 1024)
 	if cfg.Runtime.DEBUG {
 		transport.logs = logger.NewLogger("2006/01/02 15:03:04.000", logger.DEBUG, f)
 	} else {
